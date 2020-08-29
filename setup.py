1d76aed80a7433139da206223ded0337f37609d8
ChristopherLiu618 <46068662+ChristopherLiu618@users.noreply.github.com>
2020-08-29 18:44:04 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 from setuptools import setup
-import io
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 
 setup(
@@ -18,7 +10,6 @@ setup(
     author_email="yibang.christopher.liu@gmail.com",
     description="A Crunch API by Christopher",
     license='Apache',
-    long_description=long_description,
     url='https://github.com/ChristopherLiu618/CrunchAPI',
     packages=['crunchapilyb'],
     install_requires=['pandas', 'requests'],
